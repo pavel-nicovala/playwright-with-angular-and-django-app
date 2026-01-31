b02eed8cd280da2daa3dd3da529684e8b673cec8
pavel-nicovala <pnicovala@blockdaemon.com>
2026-01-31 07:29:22 +0000

    update widget styling

--- frontend/src/app/shared/pavel-widget/pavel-widget.component.ts
+++ frontend/src/app/shared/pavel-widget/pavel-widget.component.ts
@@ -5,6 +5,7 @@ import {
   OnDestroy,
   ViewChild,
 } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 import React from 'react';
 import { createRoot, Root } from 'react-dom/client';
 import { PavelWidget } from 'pavel-widget';
@@ -13,6 +14,8 @@ import { PavelWidget } from 'pavel-widget';
   selector: 'app-pavel-widget',
   standalone: true,
   template: `<div #host></div>`,
+  styleUrl: './pavel-widget.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class PavelWidgetComponent implements AfterViewInit, OnDestroy {
   @ViewChild('host', { static: true })
@@ -22,7 +25,13 @@ export class PavelWidgetComponent implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     this.reactRoot = createRoot(this.host.nativeElement);
-    this.reactRoot.render(React.createElement(PavelWidget));
+    this.reactRoot.render(
+      React.createElement(PavelWidget, {
+        className: 'conduit-pavel-widget',
+        position: 'bottom-right',
+        text: 'Pavel was here :)',
+      }),
+    );
   }
 
   ngOnDestroy(): void {
